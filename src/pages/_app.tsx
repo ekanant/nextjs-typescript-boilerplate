b33a7c77f00a014cd98dcbefa96c659b1a05f477
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-06-27 17:33:38 +0700

    add bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import App from 'next/app'
 import { appWithTranslation } from '../i18n'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 class MyApp extends App {
   render() {
