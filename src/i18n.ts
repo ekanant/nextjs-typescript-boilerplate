9b736817c740933854a08f1d60c411ba0c4ac63d
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-06-27 15:36:20 +0700

    Sample change language link

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,6 +1,6 @@
 import NextI18Next from "next-i18next";
 const localePath =  typeof window === "undefined" ? "public/static/locales" : "static/locales";
-export const nextI18Next = new NextI18Next({
+const nextI18Next = new NextI18Next({
     fallbackLng: 'th-th',
     localeSubpaths: {
         'en-th': 'en-th',
@@ -11,3 +11,16 @@ export const nextI18Next = new NextI18Next({
     localePath,
     serverLanguageDetection: false,
 })
+
+export const changeLanguage = (locale: string): void => {
+    nextI18Next.i18n.changeLanguage(locale);
+};
+  
+export const getCurrentLanguage = ():string => {
+    return nextI18Next.i18n.language
+}
+export const Link = nextI18Next.Link
+export const appWithTranslation = nextI18Next.appWithTranslation
+export const withTranslation = nextI18Next.withTranslation
+export const useTranslation = nextI18Next.useTranslation
+export default nextI18Next
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,32 @@
 import React, { ReactElement } from 'react'
-import {GetStaticProps} from 'next'
+import { GetStaticProps } from 'next'
+import Link from 'next/link'
+import { useTranslation } from '../i18n'
 
 interface Props {
-  myText: string
+  myText: string,
+  t: (arg0: string) => React.ReactNode
 }
 
-export default function index(prop: Props): ReactElement {
+const Page = function (prop: Props): ReactElement {
+  const [t, i18n] = useTranslation('content')
+  const changeLanguage = (language: string) => {
+    i18n.changeLanguage(language)
+  }
   return (
     <>
-      Hello this is typescript<br/>
-      {prop.myText}
+      Hello this is typescript<br />
+      {prop.myText}<br />
+      hello &gt; {t('hello')}<br/>
+      {t('change_language')} <Link href={{ pathname: '/', query: {} }}><a onClick={(e) => { e.preventDefault(); changeLanguage('th-th'); }}>TH</a></Link> | <Link href={{ pathname: '/en-th', query: {} }}><a onClick={(e) => { e.preventDefault(); changeLanguage('en-th'); }}>EN</a></Link><br />
+
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps  = async ()=>{
+export default Page
+
+export const getStaticProps: GetStaticProps = async () => {
   return {
     props: {
       myText: 'This is my text'
