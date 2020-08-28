e37f0da59b6c30756ae10522562298cec709d0aa
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-08-28 21:58:01 +0700

    ⚡️ Remove getInitialProps for m=better oerfirnabce

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import App from 'next/app'
-import { appWithTranslation } from '../i18n'
 import "bootstrap/dist/css/bootstrap.min.css"
+import React from 'react'
+import { appWithTranslation } from '../i18n'
 
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />
-
-MyApp.getInitialProps = async (appContext) => ({ ...await App.getInitialProps(appContext) })
 
 export default appWithTranslation(MyApp)
