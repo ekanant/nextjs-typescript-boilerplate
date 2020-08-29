0974a5d299bb0d8708ecffd627cc9187e68e0555
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-08-29 10:43:39 +0700

    Add config basePath for nexti18in

--- next.config.js
+++ next.config.js
@@ -2,15 +2,17 @@ const { nextI18NextRewrites } = require('next-i18next/rewrites')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-
+const basePath = ''
 const localeSubpaths = {
   'en-th': 'en-th',
 };
 
 module.exports = withBundleAnalyzer({
+  basePath,
   rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     localeSubpaths,
+    basePath
   },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
