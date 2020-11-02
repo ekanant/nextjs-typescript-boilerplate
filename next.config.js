36a39aa32ebc4bfe155e119ae1aa5610399ff32f
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-11-02 17:16:24 +0700

    Simple i18n from next

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
-const { nextI18NextRewrites } = require('next-i18next/rewrites')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 const basePath = ''
-const localeSubpaths = {
-  'en-th': 'en-th',
-};
 
 module.exports = withBundleAnalyzer({
   basePath,
-  rewrites: async () => nextI18NextRewrites(localeSubpaths),
-  publicRuntimeConfig: {
-    localeSubpaths,
-    basePath
+  i18n: {
+    locales: ['en-th', 'th-th'],
+    defaultLocale: 'th-th',
   },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
