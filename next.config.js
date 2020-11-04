c9e6a1429f035317efad1ba7c92b9b73d3bfe1f5
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2020-11-04 16:07:27 +0700

    Use preact instead react

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
+
+const withPreact = require('next-plugin-preact');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 const basePath = ''
-
-module.exports = withBundleAnalyzer({
+const nextJsConfig = {
   basePath,
   i18n: {
     locales: ['en-th', 'th-th'],
@@ -17,4 +18,8 @@ module.exports = withBundleAnalyzer({
 
     return config
   }
-})
+}
+
+module.exports = withBundleAnalyzer(
+  withPreact(nextJsConfig)
+)
